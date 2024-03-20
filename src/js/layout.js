26bdd6cbf7da60190bf3bc31d984c37ac20d0bf5
Seba Fretes <84410616+SebaFretes@users.noreply.github.com>
2024-03-20 14:53:13 +0000

    characters view done

--- src/js/layout.js
+++ src/js/layout.js
@@ -24,7 +24,7 @@ const Layout = () => {
 					<Routes>
 						<Route path="/" element={<Home />} />
 						<Route path="/demo" element={<Demo />} />
-						<Route path="/characters/:theid" element={<Characters />} />
+						<Route path="/characters/:id" element={<Characters />} />
 						<Route path="*" element={<h1>Not found!</h1>} />
 					</Routes>
 					<Footer />
--- src/js/views/characters.js
+++ src/js/views/characters.js
@@ -1,26 +1,36 @@
 import React, { useState, useEffect, useContext } from "react";
-import PropTypes from "prop-types";
 import { Link, useParams } from "react-router-dom";
 import { Context } from "../store/appContext";
 
-export const Characters = props => {
-	const { store, actions } = useContext(Context);
-	const params = useParams();
+export const Characters = () => {
+	// const { store, actions } = useContext(Context);
+	const [character, setCharacter] = useState(null);
+	const { id } = useParams();
+
+	const fetchEverySingleCharacter = async () => {
+		const res = await fetch(`https://www.swapi.tech/api/people/${id}`)
+		const data = await res.json()
+		setCharacter(data.result);
+	}
+
+	useEffect(() => {
+		fetchEverySingleCharacter();
+	}, [])
+
 	return (
-		<div className="jumbotron">
-			<h1 className="display-4">This will show the demo element: {store.demo[params.theid].title}</h1>
-
-			<hr className="my-4" />
-
-			<Link to="/">
-				<span className="btn btn-primary btn-lg" href="#" role="button">
-					Back home
-				</span>
-			</Link>
+		<div className="container-fluid vh-auto d-flex align-items-center justify-content-center">
+			<div className="col">
+				<img src={`https://starwars-visualguide.com/assets/img/characters/${id}.jpg`} className="card-img-top" alt="..." />
+			</div>
+			<div className="col">
+				{
+					character &&
+					<div className="text-center">
+						<h5 className="card-title text-center" style={{ textAlign: 'start' }}>{character.properties.name}</h5>
+						<p className="card-text text-center" style={{ textAlign: 'start' }}>{character.description}</p>
+					</div>
+				}
+			</div>
 		</div>
 	);
-};
-
-Characters.propTypes = {
-	match: PropTypes.object
 };
