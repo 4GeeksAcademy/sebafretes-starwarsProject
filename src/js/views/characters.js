fe54eb45cb32bd7c6e374a0ad3dceda24ea5c25e
Seba Fretes <84410616+SebaFretes@users.noreply.github.com>
2024-03-30 17:15:22 +0000

    descriptions added on planets.js view

--- src/js/views/characters.js
+++ src/js/views/characters.js
@@ -60,7 +60,6 @@ export const Characters = () => {
 					}
 				</div>
 			</div>
-
 		</>
 	);
 };
--- src/js/views/planets.js
+++ src/js/views/planets.js
@@ -16,21 +16,50 @@ export const Planets = () => {
 		fetchEveryPlanet();
 	}, [])
 
-
 	return (
-		<div className="container-fluid d-flex align-items-center justify-content-center" style={{ height: '100vh' }}>
-			<div className="col text-center">
-				<img src={`https://starwars-visualguide.com/assets/img/planets/${id}.jpg`} className="card-img-top img-fluid" alt="..." style={{ maxWidth: '50%', height: 'auto' }} />
-			</div>
-			<div className="col">
-				{
-					planet &&
-					<div className="text-center">
-						<h5 className="card-title text-center" style={{ textAlign: 'start' }}>{planet.properties.name}</h5>
-						<p className="card-text text-center" style={{ textAlign: 'start' }}>{planet.properties.terrain}</p>
+		<>
+			<div className="container">
+				<div className="container-fluid d-flex align-items-center justify-content-center" style={{ height: '100vh' }}>
+					<div className="col text-center">
+						<img src={`https://starwars-visualguide.com/assets/img/planets/${id}.jpg`} className="card-img-top img-fluid" alt="..." style={{ maxWidth: '50%', height: 'auto' }} />
 					</div>
-				}
+					<div className="col">
+						{
+							planet &&
+							<div className="text-center">
+								<h5 className="card-title text-center" style={{ textAlign: 'start' }}>{planet.properties.name}</h5>
+								<p className="card-text text-center" style={{ textAlign: 'start' }}>{planet.description}</p>
+							</div>
+						}
+					</div>
+				</div>
+				<hr style={{ color: 'red' }} ></hr>
+				<div className="d-flex justify-content-center text-center">
+					{
+						planet &&
+						<>
+							<p style={{ color: '#E20B49', fontWeight: '600', marginRight: '2rem' }}>Name
+								<p style={{ color: '#E20B49', fontWeight: '400' }}>{planet.properties.name}</p>
+							</p>
+							<p style={{ color: '#E20B49', fontWeight: '600', marginRight: '2rem' }}>Climate
+								<p style={{ color: '#E20B49', fontWeight: '400' }}>{planet.properties.climate}</p>
+							</p>
+							<p style={{ color: '#E20B49', fontWeight: '600', marginRight: '2rem' }}>Population
+								<p style={{ color: '#E20B49', fontWeight: '400' }}>{planet.properties.population}</p>
+							</p>
+							<p style={{ color: '#E20B49', fontWeight: '600', marginRight: '2rem' }}>Orbital Period
+								<p style={{ color: '#E20B49', fontWeight: '400' }}>{planet.properties.orbital_period}</p>
+							</p>
+							<p style={{ color: '#E20B49', fontWeight: '600', marginRight: '2rem' }}>Rotation Period
+								<p style={{ color: '#E20B49', fontWeight: '400' }}>{planet.properties.rotation_period}</p>
+							</p>
+							<p style={{ color: '#E20B49', fontWeight: '600', marginRight: '2rem' }}>Diameter
+								<p style={{ color: '#E20B49', fontWeight: '400' }}>{planet.properties.diameter}</p>
+							</p>
+						</>
+					}
+				</div>
 			</div>
-		</div>
+		</>
 	);
 };
